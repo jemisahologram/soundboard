7efe093217c4e8241e199f3aba3a9cf74794b1fe
jemisahologram <55041950+jemisahologram@users.noreply.github.com>
2020-09-09 07:35:25 +1000

    Add files via upload

---
+++ soundboard/soundboard.js
@@ -0,0 +1,141 @@
+document.addEventListener ('keydown', function(e) {
+//QWERTYUIOP - line 1
+//immanence becomes imminence the ash of annihilation lands softly on
+//Q -- immanence
+
+
+if (e.keyCode == 81) {
+  document.getElementById('immanence').play()
+  document.getElementById("words").innerHTML = "immanence"
+}
+
+//W -- becomes
+  if (e.keyCode == 87) {
+    document.getElementById('becomes').play()
+    document.getElementById("words").innerHTML = "becomes"
+  }
+//E -- imminence
+  if (e.keyCode == 69) {
+      document.getElementById('imminence').play()
+      document.getElementById("words").innerHTML = "imminence"
+  }
+//R -- the
+  if (e.keyCode == 82) {
+    document.getElementById('the1').play()
+    document.getElementById("words").innerHTML = "the"
+  }
+//T -- ash
+  if (e.keyCode == 84) {
+    document.getElementById('ash').play()
+    document.getElementById("words").innerHTML = "ash"
+  }
+//Y -- of
+  if (e.keyCode == 89) {
+    document.getElementById('of').play()
+    document.getElementById("words").innerHTML = "of"
+  }
+//U -- annihilation
+  if (e.keyCode == 85) {
+    document.getElementById('annihilation').play()
+    document.getElementById("words").innerHTML = "annihilation"
+  }
+//I -- lands
+  if (e.keyCode == 73) {
+    document.getElementById('lands').play()
+    document.getElementById("words").innerHTML = "lands"
+  }
+//O -- softly
+ if (e.keyCode == 79) {
+   document.getElementById('softly').play()
+   document.getElementById("words").innerHTML = "softly"
+ }
+//P -- on
+  if (e.keyCode == 80) {
+    document.getElementById('on').play()
+    document.getElementById("words").innerHTML = "on"
+  }
+//ASDFGHJKL - line 2
+//lungs of those watching the sun darkens in its
+//A -- lungs
+  if (e.keyCode == 65) {
+    document.getElementById('lungs').play()
+    document.getElementById("words").innerHTML = "lungs"
+  }
+//S -- of
+  if (e.keyCode == 83) {
+    document.getElementById('of').play()
+    document.getElementById("words").innerHTML = "of"
+  }
+//D -- those
+  if (e.keyCode == 68) {
+    document.getElementById('those').play()
+    document.getElementById("words").innerHTML = "those"
+  }
+//F-- watching
+  if (e.keyCode == 70) {
+    document.getElementById('watching').play()
+    document.getElementById("words").innerHTML = "watching"
+    }
+//G -- the
+  if (e.keyCode == 71) {
+    document.getElementById('the2').play()
+    document.getElementById("words").innerHTML = "the"
+  }
+//H -- sun
+  if (e.keyCode == 72) {
+    document.getElementById('sun').play()
+    document.getElementById("words").innerHTML = "sun"
+  }
+//J -- darkens
+  if (e.keyCode == 74) {
+    document.getElementById('darkens').play()
+    document.getElementById("words").innerHTML = "darkens"
+    }
+//K -- in
+  if (e.keyCode == 75) {
+    document.getElementById('in').play()
+    document.getElementById("words").innerHTML = "in"
+    }
+//L -- its
+  if (e.keyCode == 76) {
+    document.getElementById('its').play()
+    document.getElementById("words").innerHTML = "its"
+    }
+//ZXCVBNM - line 3
+//brightness I am already word without end
+//Z -- brightness
+  if (e.keyCode == 90) {
+    document.getElementById('brightness').play()
+    document.getElementById("words").innerHTML = "brightness"
+    }
+//X -- I
+  if (e.keyCode == 88) {
+    document.getElementById('I').play()
+    document.getElementById("words").innerHTML = "I"
+    }
+//C -- am
+  if (e.keyCode == 67) {
+    document.getElementById('am').play()
+    document.getElementById("words").innerHTML = "am"
+    }
+//V -- already
+  if (e.keyCode == 86) {
+    document.getElementById('already').play()
+    document.getElementById("words").innerHTML = "already"
+    }
+//B -- word
+  if (e.keyCode == 66) {
+    document.getElementById('word').play()
+    document.getElementById("words").innerHTML = "word"
+    }
+//N -- without
+  if (e.keyCode == 78) {
+    document.getElementById('without').play()
+    document.getElementById("words").innerHTML = "without"
+    }
+//M -- end
+  if (e.keyCode == 77) {
+    document.getElementById('end').play()
+    document.getElementById("words").innerHTML = "end"
+    }
+})
